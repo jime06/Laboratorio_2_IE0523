a6c46c3c8e4112316345ddd082f54d81902fd58d
jime06 <jimegonzalezj@outlook.com>
2024-05-16 10:03:21 -0600

    se termina el tester

--- tester_laboratorio2.v
+++ tester_laboratorio2.v
@@ -38,7 +38,191 @@ initial begin
     #1 reset = 0;
     #1 reset = 1;
     tarjeta_recibida = 1;
-    //se prueba el recibido de tarjeta
+    pin = 'h2023;
 
+    //se prueba el primer pin
+    //se ingresa el primer dígito
+    #3 digito = 1;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 3;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 4;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //se ingresa el segundo pin
+    #3 digito = 1;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 1;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 1;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 1;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer pin
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //se bloquea el cajero y se reinicia
+    #5 reset = 0;
+    #5 reset = 1;
+
+    //se ingresa el pin verdadero
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 3;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+    //se autentica el usuario
+
+    //se prueba el depósito
+    tipo_trans = 0;
+
+    //se ingresa el monto a depositar
+    #3 monto = 100000;
+    #1 monto_stb = 1;
+    #monto_stb = 0;
+    monto = 0;
+
+    //se prueba el retiro
+    #5 tipo_trans = 0;
+
+    //se reinicia el cajero
+    #5 reset = 0;
+    #5 reset = 1;
+
+    //se ingresa el pin correcto
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 3;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //se intenta un retiro con fondos insuficientes
+    #3 monto = 1000000000;
+    #1 monto_stb = 1;
+    #1 monto_stb = 0;
+    monto = 0;
+
+    //se vuelve a ingresar el usuario y se reinicia el cajero con el tipo de transacción
+    #5 tipo_trans = 1;
+    #5 reset = 0;
+    #5 reset = 1;
+
+    //ingreso del código
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //segundo dígito
+    #3 digito = 0;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //tercer dígito
+    #3 digito = 2;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+
+    //cuarto dígito
+    #3 digito = 3;
+    #1 digito_stb = 1;
+    #1 digito_stb = 0;
+    digito = 0;
+    //se autentica el usuario y se procede con la transacción
+    #3 monto = 9100;
+    #1 monto_stb = 1;
+    31 monto_stb = 0;
+    monto = 0;
+
+    #10;
+    $finish;
 end
 endmodule
